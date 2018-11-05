5d7e253eee60d344c7306e919734fa886a83a4e1
Jack Porter <AFKmuch@users.noreply.github.com>
2018-11-05 14:49:29 +0000

    End of lesson

---
+++ Capitalist's Cupidity/Assets/Scripts/Facility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Facility : MonoBehaviour {
+    //Public
+    public enum FacilityType { Catering, CarPark, Toilets, Insurance, Security, Hardware, Christmas, Wages, Gym }
+    public float costToBuy;
+    public float baseMonthlyExpenses;
+
+    //Private
+    private float fundingPercentage;
+    private float averageEmployeeHappiness;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Capitalist's Cupidity/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerStats : MonoBehaviour
+{
+    //Public
+    public float startingCompanyFunds = 10000;
+    public float startingIncomeKept = 0.2f;
+
+    //Private
+    private float personalFunds;
+    private float companyFunds;
+    private float incomeKeepPercent;
+    private int employeeCount;
+
+    //References
+    private Text personalFundsTextRef;
+    private Text companyFundsTextRef;
+    private Text incomeKeptPercentTextRef;
+    private Text employeeCountTextRef;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //initalise references
+        personalFundsTextRef = GameObject.Find("PersonalFundsText").GetComponent<Text>();
+        companyFundsTextRef = GameObject.Find("CompanyFundsText").GetComponent<Text>();
+        incomeKeptPercentTextRef = GameObject.Find("IncomeKeptPercentText").GetComponent<Text>();
+        employeeCountTextRef = GameObject.Find("EmployeeCountText").GetComponent<Text>();
+
+        companyFunds = startingCompanyFunds;
+        incomeKeepPercent = startingIncomeKept;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        updateUI();
+	}
+
+    public void updateUI()
+    {
+        personalFundsTextRef.text = "Personal Funds: $" + personalFunds.ToString();
+        companyFundsTextRef.text = "Company Funds: $" + companyFunds.ToString();
+        incomeKeptPercentTextRef.text = "Income Kept: " + (incomeKeepPercent * 100).ToString() + "%";
+        employeeCountTextRef.text = "# Of Employees: " + employeeCount.ToString();
+    }
+}
