e00644759616965a6ccad3ed94dba95f698be9e9
Edwardjsmith <2smite17@solent.ac.uk>
2018-11-05 14:27:33 +0000

    Started setting up employees

---
+++ Capitalist's Cupidity/Assets/Scripts/Employees/Director.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour
+{
+    public Employee employeePrefab;
+    List<Employee> employees;
+    public int employeePoolCount = 20;
+
+	// Use this for initialization
+	void Start ()
+    {
+        employees = new List<Employee>();
+
+        for(int i = 0; i < employeePoolCount; i++)
+        {
+            employeePrefab = Instantiate(employeePrefab);
+            employeePrefab.gameObject.SetActive(false);
+            employees.Add(employeePrefab);
+            employeePoolCount++;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void setEmployeeTargetPos()
+    {
+
+    }
+
+    void flockingManager()
+    { }
+}
