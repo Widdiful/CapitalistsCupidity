8b808ef08778258f8900fbed04d0ffe9093a0736
Widdiful <Widdiful@users.noreply.github.com>
2018-11-11 18:49:54 +0000

    Added loading screen

---
+++ Capitalist's Cupidity/Assets/Scripts/System/LoadLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour {
+
+    public Canvas loadingCanvas;
+    public Slider loadingBar;
+
+    public void Load(int sceneIndex) {
+        if (loadingCanvas)
+            loadingCanvas.enabled = true;
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+	IEnumerator LoadAsync(int sceneIndex) {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!loading.isDone) {
+            float progress = Mathf.Clamp01(loading.progress / 0.9f);
+
+            if (loadingBar)
+                loadingBar.value = progress;
+
+            yield return null;
+        }
+    }
+}
