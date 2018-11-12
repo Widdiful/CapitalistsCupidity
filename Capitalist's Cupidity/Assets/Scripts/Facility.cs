1f8f34865a638516149e8cf9e5793b84b6ccf8a1
Jack Porter <AFKmuch@users.noreply.github.com>
2018-11-12 14:58:25 +0000

    You can edit the facility now

--- Capitalist's Cupidity/Assets/Scripts/Facility.cs
+++ Capitalist's Cupidity/Assets/Scripts/Facility.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Facility : MonoBehaviour {
+public class Facility : MonoBehaviour
+{
     //Public
     public enum FacilityType { Catering, CarPark, Toilets, Insurance, Security, Hardware, Christmas, Wages, Gym }
     public float costToBuy;
@@ -20,16 +21,25 @@ public class Facility : MonoBehaviour {
 	void Start ()
     {
         FacilityCanvas = GameObject.Find("FacilityCanvas");
+        fundingPercentage = 1;
 	}
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            OpenFacilityWindow();
+        }
+    }
 
     private void CalculateAverageEmployeeHappiness()
     {
 
     }
 
-    private void CutFacility()
+    public void CutFacility()
     {
-
+        // Cut facility here
     }
 
     private void BuyFacility()
@@ -40,18 +50,15 @@ public class Facility : MonoBehaviour {
     private void OpenFacilityWindow()
     {
         CalculateAverageEmployeeHappiness();
-        FacilityCanvas.transform.GetChild(0).Find("ConfirmButton").GetComponent<Button>().onClick.RemoveAllListeners();
-        FacilityCanvas.GetComponent<Canvas>().enabled = true;
-        FacilityCanvas.transform.GetChild(0).Find("FacilityNameText").GetComponent<Text>().text = facilityName;
-        FacilityCanvas.transform.GetChild(0).Find("FundingText").GetComponent<Text>().text = (baseMonthlyExpenses * fundingPercentage).ToString();
-        FacilityCanvas.transform.GetChild(0).Find("FundingSlider").GetComponent<Slider>().value = fundingPercentage;
-        FacilityCanvas.transform.GetChild(0).Find("HappinessSlider").GetComponent<Slider>().value = averageEmployeeHappiness;
-        FacilityCanvas.transform.GetChild(0).Find("ConfirmButton").GetComponent<Button>().onClick.AddListener(this.confirmChanges);
+        FacilityCanvas.GetComponent<FacilityCanvas>().OpenFacilityWindow(this, facilityName, baseMonthlyExpenses, fundingPercentage, averageEmployeeHappiness); // Open the facility window and populate the values
     }
 
-    private void confirmChanges()
+    public void UpdateFromFacilityWindow(float FundingPercentage)
     {
-        fundingPercentage = FacilityCanvas.transform.GetChild(0).Find("FundingSlider").GetComponent<Slider>().value;
-        FacilityCanvas.GetComponent<Canvas>().enabled = false;
+        fundingPercentage = FundingPercentage; // Set funding value based on slider position
+        if (fundingPercentage <= 0)
+        {
+            CutFacility(); // if the funding percentage is now 0, then cut the facility.
+        }
     }
 }
---
+++ Capitalist's Cupidity/Assets/Scripts/FacilityCanvas.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FacilityCanvas : MonoBehaviour
+{
+    Text facilityNameText;
+    Text fundingText;
+    Slider fundingSlider;
+    Slider happinessSlider;
+    Button confirmButton;
+    Button cancelButton;
+    Facility selectedFacility;
+    string facilityName;
+    float funding;
+    float baseExpense;
+    float fundingPercent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        facilityNameText = transform.GetChild(0).Find("FacilityNameText").GetComponent<Text>();
+        fundingText = transform.GetChild(0).Find("FundingText").GetComponent<Text>();
+        fundingSlider = transform.GetChild(0).Find("FundingSlider").GetComponent<Slider>();
+        happinessSlider = transform.GetChild(0).Find("HappinessSlider").GetComponent<Slider>();
+        confirmButton = transform.GetChild(0).Find("ConfirmButton").GetComponent<Button>();
+        cancelButton = transform.GetChild(0).Find("CancelButton").GetComponent<Button>();
+
+        cancelButton.onClick.AddListener(CancelChanges);
+        confirmButton.onClick.AddListener(ConfirmChanges);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(fundingPercent != fundingSlider.value)
+        {
+            fundingPercent = fundingSlider.value;
+            UpdateUI();
+        }
+	}
+
+    public void OpenFacilityWindow(Facility SelectedFacility, string FacilityName, float BaseExpense, float FundingPercent, float AverageHappiness)
+    {
+        selectedFacility = SelectedFacility; // Set the selected Facility
+        facilityName = FacilityName;
+        baseExpense = BaseExpense; // Set the base expense
+        fundingPercent = FundingPercent; // Set the funding percent
+        funding = (baseExpense * fundingPercent); // Set the funding amount
+
+        GetComponent<Canvas>().enabled = true; // Open the canvas
+
+        facilityNameText.text = facilityName; // Set facility name on canvas
+
+
+        fundingText.text = "$" + funding.ToString() + " p/m"; // Set funding number on canvas
+
+        fundingSlider.value = fundingPercent; // Set funding slider position on canvas
+
+        happinessSlider.value = AverageHappiness; // Set happiness slider position on canvas
+    }
+
+    private void UpdateUI()
+    {
+        funding = (baseExpense * fundingPercent); // Set the funding amount
+        fundingText.text = "$" + funding.ToString("0.00") + " p/m"; // Set funding number on canvas
+        if(funding <= 0)
+        {
+            fundingText.text = "Cut Facility";
+        }
+    }
+
+    private void ConfirmChanges()
+    {
+        selectedFacility.UpdateFromFacilityWindow(fundingPercent); // Update the facility with the new values
+        GetComponent<Canvas>().enabled = false; // Close the canvas
+    }
+
+    private void CancelChanges()
+    {
+        selectedFacility = null; // Deselect the facility
+        GetComponent<Canvas>().enabled = false; // Close the canvas
+    }
+}
