483166f99f89490d757d97a7d31bb68d13ef22c7
Jack Porter <AFKmuch@users.noreply.github.com>
2018-11-12 13:54:35 +0000

    Facility Canvas should update with facility information now

--- Capitalist's Cupidity/Assets/Scripts/Facility.cs
+++ Capitalist's Cupidity/Assets/Scripts/Facility.cs
@@ -1,27 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Facility : MonoBehaviour {
     //Public
     public enum FacilityType { Catering, CarPark, Toilets, Insurance, Security, Hardware, Christmas, Wages, Gym }
     public float costToBuy;
     public float baseMonthlyExpenses;
+    public string facilityName;
+    public FacilityType facilityType;
 
     //Private
     private float fundingPercentage;
     private float averageEmployeeHappiness;
+    private GameObject FacilityCanvas;
 
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
+        FacilityCanvas = GameObject.Find("FacilityCanvas");
 	}
 
     private void CalculateAverageEmployeeHappiness()
@@ -41,7 +39,19 @@ public class Facility : MonoBehaviour {
 
     private void OpenFacilityWindow()
     {
-
+        CalculateAverageEmployeeHappiness();
+        FacilityCanvas.transform.GetChild(0).Find("ConfirmButton").GetComponent<Button>().onClick.RemoveAllListeners();
+        FacilityCanvas.GetComponent<Canvas>().enabled = true;
+        FacilityCanvas.transform.GetChild(0).Find("FacilityNameText").GetComponent<Text>().text = facilityName;
+        FacilityCanvas.transform.GetChild(0).Find("FundingText").GetComponent<Text>().text = (baseMonthlyExpenses * fundingPercentage).ToString();
+        FacilityCanvas.transform.GetChild(0).Find("FundingSlider").GetComponent<Slider>().value = fundingPercentage;
+        FacilityCanvas.transform.GetChild(0).Find("HappinessSlider").GetComponent<Slider>().value = averageEmployeeHappiness;
+        FacilityCanvas.transform.GetChild(0).Find("ConfirmButton").GetComponent<Button>().onClick.AddListener(this.confirmChanges);
     }
 
+    private void confirmChanges()
+    {
+        fundingPercentage = FacilityCanvas.transform.GetChild(0).Find("FundingSlider").GetComponent<Slider>().value;
+        FacilityCanvas.GetComponent<Canvas>().enabled = false;
+    }
 }
