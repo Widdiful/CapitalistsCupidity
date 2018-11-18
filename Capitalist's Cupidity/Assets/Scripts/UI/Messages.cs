4979e4dcab910c93b03bd5c58ae4fe3e255d66a6
Widdiful <Widdiful@users.noreply.github.com>
2018-11-18 18:14:32 +0000

    Ticker is functionally finished

--- Capitalist's Cupidity/Assets/Scripts/UI/Messages.cs
+++ Capitalist's Cupidity/Assets/Scripts/UI/Messages.cs
@@ -5,31 +5,51 @@ using UnityEngine.UI;
 
 public class Messages : MonoBehaviour {
 
+    public enum MessageType { Ticker, Noticeboard, Both };
     public Text tickerMessage;
+    public RectTransform tickerCanvas;
     public float speed;
 
+    private Queue<string> messageQueue = new Queue<string>();
     private RectTransform tickerRect;
-    private RectTransform tickerCanvas;
     private Vector2 tickerStart;
+    private Animator anim;
 
-    private bool tickerFinished;
+    private bool tickerFinished = true;
 
     void Start() {
         if (tickerMessage) {
             tickerRect = tickerMessage.rectTransform;
             tickerStart = tickerRect.anchoredPosition;
-            tickerCanvas = tickerMessage.canvas.GetComponent<RectTransform>();
         }
         else
             Debug.LogWarning("Cannot set tickerRect as tickerMessage is null.");
 
-        CreateTicker("Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aliquam quis leo vel ex egestas aliquam.Vivamus dignissim sed nisi in molestie.Nulla lacinia non sapien quis eleifend.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Nam vel tincidunt tortor.Mauris tempus mi diam, et ultrices urna lobortis eget.Sed maximus vehicula ipsum, non molestie magna.Praesent sed eros sed enim tempus dictum. Vivamus et efficitur lectus, eget faucibus arcu.Nunc pretium, orci non tempor pretium, nulla leo mattis lectus, eget maximus eros ante non justo.Pellentesque dapibus urna vitae maximus porttitor.Praesent et neque eget augue placerat mattis.Maecenas malesuada, arcu non elementum molestie, erat mi consectetur nibh, at dictum magna lorem et augue.Etiam tristique imperdiet elit, at vulputate turpis porta non.Phasellus posuere et augue eget viverra.Etiam vitae lorem vitae diam auctor efficitur id eget tellus.Morbi vehicula ac nisi id lacinia.Etiam blandit lectus non faucibus sollicitudin. Nulla faucibus, dui sed convallis tristique, quam turpis pretium dolor, sit amet fringilla eros velit a nisl.Curabitur eros dolor, posuere at mauris vel, viverra vulputate justo.Proin varius lacinia turpis, nec interdum diam facilisis at.Nam tincidunt massa at nulla tristique, id volutpat mauris euismod.Fusce sagittis bibendum aliquet.Phasellus a eros tincidunt, cursus libero sit amet, dignissim nisi.Suspendisse malesuada ultricies neque in vestibulum.Duis non cursus nibh, non sodales ante.Pellentesque rhoncus fringilla nisl, lobortis luctus metus maximus non.Donec fringilla arcu vel consectetur pulvinar.Quisque nisl erat, mollis et lacus eu, faucibus viverra lacus.Nunc lacinia efficitur suscipit.Fusce non magna sed lectus euismod rhoncus.Aliquam id pharetra felis. Ut faucibus enim purus, ac finibus dolor tempor ac.Sed elementum lectus id pretium blandit.Praesent vel sodales felis, in cursus risus.In efficitur suscipit bibendum.Donec sodales luctus faucibus.Nullam posuere efficitur odio, non semper nisl elementum ut.Duis eleifend lectus consectetur nisl pretium dictum.Mauris eget nibh mollis, condimentum tellus at, congue ex.Vestibulum placerat ex in sem sagittis, id volutpat erat sollicitudin.Sed congue, dui ut feugiat fringilla, nisi turpis euismod purus, sed pharetra quam ex nec ligula.Donec in nunc lectus.Vestibulum accumsan dictum nulla, a blandit orci aliquam quis. Maecenas vitae dignissim justo.Aenean ac consequat odio.Cras a est quam.Proin at suscipit magna.Vivamus fermentum nec lacus ac ornare.Curabitur volutpat nulla nisi, sit amet iaculis eros eleifend a.Donec turpis erat, rhoncus eget elit quis, iaculis vestibulum justo.Vivamus gravida, orci vitae vestibulum mollis, ligula nulla laoreet mi, non luctus felis lorem vitae mi.Phasellus ullamcorper pharetra risus, a laoreet nisl auctor vitae. ");
+        anim = tickerMessage.transform.parent.GetComponent<Animator>();
+
+        NewMessage("Test1", MessageType.Ticker);
+        NewMessage("Test2", MessageType.Ticker);
+        NewMessage("Test3", MessageType.Ticker);
+        //CreateTicker("Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aliquam quis leo vel ex egestas aliquam.Vivamus dignissim sed nisi in molestie.Nulla lacinia non sapien quis eleifend.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Nam vel tincidunt tortor.Mauris tempus mi diam, et ultrices urna lobortis eget.Sed maximus vehicula ipsum, non molestie magna.Praesent sed eros sed enim tempus dictum. Vivamus et efficitur lectus, eget faucibus arcu.Nunc pretium, orci non tempor pretium, nulla leo mattis lectus, eget maximus eros ante non justo.Pellentesque dapibus urna vitae maximus porttitor.Praesent et neque eget augue placerat mattis.Maecenas malesuada, arcu non elementum molestie, erat mi consectetur nibh, at dictum magna lorem et augue.Etiam tristique imperdiet elit, at vulputate turpis porta non.Phasellus posuere et augue eget viverra.Etiam vitae lorem vitae diam auctor efficitur id eget tellus.Morbi vehicula ac nisi id lacinia.Etiam blandit lectus non faucibus sollicitudin. Nulla faucibus, dui sed convallis tristique, quam turpis pretium dolor, sit amet fringilla eros velit a nisl.Curabitur eros dolor, posuere at mauris vel, viverra vulputate justo.Proin varius lacinia turpis, nec interdum diam facilisis at.Nam tincidunt massa at nulla tristique, id volutpat mauris euismod.Fusce sagittis bibendum aliquet.Phasellus a eros tincidunt, cursus libero sit amet, dignissim nisi.Suspendisse malesuada ultricies neque in vestibulum.Duis non cursus nibh, non sodales ante.Pellentesque rhoncus fringilla nisl, lobortis luctus metus maximus non.Donec fringilla arcu vel consectetur pulvinar.Quisque nisl erat, mollis et lacus eu, faucibus viverra lacus.Nunc lacinia efficitur suscipit.Fusce non magna sed lectus euismod rhoncus.Aliquam id pharetra felis. Ut faucibus enim purus, ac finibus dolor tempor ac.Sed elementum lectus id pretium blandit.Praesent vel sodales felis, in cursus risus.In efficitur suscipit bibendum.Donec sodales luctus faucibus.Nullam posuere efficitur odio, non semper nisl elementum ut.Duis eleifend lectus consectetur nisl pretium dictum.Mauris eget nibh mollis, condimentum tellus at, congue ex.Vestibulum placerat ex in sem sagittis, id volutpat erat sollicitudin.Sed congue, dui ut feugiat fringilla, nisi turpis euismod purus, sed pharetra quam ex nec ligula.Donec in nunc lectus.Vestibulum accumsan dictum nulla, a blandit orci aliquam quis. Maecenas vitae dignissim justo.Aenean ac consequat odio.Cras a est quam.Proin at suscipit magna.Vivamus fermentum nec lacus ac ornare.Curabitur volutpat nulla nisi, sit amet iaculis eros eleifend a.Donec turpis erat, rhoncus eget elit quis, iaculis vestibulum justo.Vivamus gravida, orci vitae vestibulum mollis, ligula nulla laoreet mi, non luctus felis lorem vitae mi.Phasellus ullamcorper pharetra risus, a laoreet nisl auctor vitae. ");
     }
 
-    public void CreateTicker(string message) {
+    public void NewMessage(string message, MessageType messageType) {
+        if (messageType != MessageType.Noticeboard) {
+            messageQueue.Enqueue(message);
+            if (tickerFinished) {
+                CreateTicker(messageQueue.Dequeue());
+            }
+        }
+        if (messageType != MessageType.Ticker) {
+
+        }
+    }
+
+    private void CreateTicker(string message) {
         if (tickerMessage) {
             tickerMessage.text = message;
 
+            anim.SetBool("Open", true);
             StartCoroutine(MoveTicker());
         }
         else
@@ -55,6 +75,12 @@ public class Messages : MonoBehaviour {
             tickerFinished = true;
 
             // Check queue for more messages
+            if (messageQueue.Count > 0) {
+                CreateTicker(messageQueue.Dequeue());
+            }
+            else {
+                anim.SetBool("Open", false);
+            }
         }
         else
             Debug.LogWarning("Cannot move ticker as tickerMessage is null.");
