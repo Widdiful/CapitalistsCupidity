02180b9172a18a706b13357997f65161aac0cac7
Edward <spottablefiend@gmail.com>
2018-12-02 09:09:06 +0000

    Added basic saving and loading for director. will extend to everything once I work out whatr needs saving

---
+++ Capitalist's Cupidity/Assets/Scripts/System/savingAndLoading.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+
+public class SaveData
+{
+    public List<Employee> employees;
+    public int numberOfMonths;
+}
+
+public class savingAndLoading : MonoBehaviour
+{
+    Director directorScript;
+    PlayerStats playerStatScript;
+    SaveData saveData;
+
+    private void Start()
+    {
+        saveData = new SaveData();
+        directorScript = GameObject.Find("Director").GetComponent<Director>();
+    }
+
+
+
+    public void save()
+    {
+        readData();
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/saveFile.dat");
+        bf.Serialize(file, saveData);
+        file.Close();
+    }
+
+    public void load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/saveFile.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/saveFile.dat", FileMode.Open);
+            saveData = (SaveData)bf.Deserialize(file);
+            file.Close();
+            writeData();
+            Debug.Log("Save loaded!");
+        }
+        else
+        {
+            Debug.Log("No save found!");
+        }
+    }
+
+    public void readData()
+    {
+        saveData.employees = directorScript.employees;
+        saveData.numberOfMonths = directorScript.numberOfMonths;
+    }
+
+    public void writeData()
+    {
+        directorScript.employees = saveData.employees;
+        directorScript.numberOfMonths = saveData.numberOfMonths;
+    }
+}
