9a44b712b2bf27e4f716effc3d5debedae5ea5d6
Widdiful <Widdiful@users.noreply.github.com>
2018-12-19 20:54:13 +0000

    Started leaderboard table

---
+++ Capitalist's Cupidity/Assets/Scripts/UI/HighScoreManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreManager : MonoBehaviour {
+
+    public Transform content;
+    public GameObject leaderboardItemPrefab;
+
+    public enum GameTypes { Time, Gold, Free };
+    public GameTypes gameType;
+
+    public enum ScoreTypes { Local, Global };
+    public ScoreTypes scoreType;
+
+    public void GameTime() {
+        gameType = GameTypes.Time;
+        LoadScores();
+    }
+
+    public void GameGold() {
+        gameType = GameTypes.Gold;
+        LoadScores();
+    }
+
+    public void GameFree() {
+        gameType = GameTypes.Free;
+        LoadScores();
+    }
+
+    public void ScoreLocal() {
+        scoreType = ScoreTypes.Local;
+        LoadScores();
+    }
+
+    public void ScoreGlobal() {
+        scoreType = ScoreTypes.Global;
+        LoadScores();
+    }
+
+    public void LoadScores() {
+        foreach (Transform button in content) {
+            Destroy(button.gameObject);
+        }
+
+        LeaderboardItem newItem = Instantiate(leaderboardItemPrefab, content).GetComponent<LeaderboardItem>();
+        if (newItem) {
+            newItem.position = 1;
+            newItem.playerName = gameType.ToString();
+            newItem.companyName = scoreType.ToString();
+            newItem.score = "$123,456,789";
+            newItem.UpdateInformation();
+        }
+    }
+}
---
+++ Capitalist's Cupidity/Assets/Scripts/UI/LeaderboardItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardItem : MonoBehaviour {
+
+    public Text positionText;
+    public Text nameText;
+    public Text companyText;
+    public Text scoreText;
+
+
+    public int position;
+    public string playerName;
+    public string companyName;
+    public string score;
+
+    public void UpdateInformation() {
+        positionText.text = "#" + position;
+        nameText.text = playerName;
+        companyText.text = companyName;
+        scoreText.text = score;
+    }
+}
